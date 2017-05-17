f33325768ef39433934f6b4296cd624e2dd8ab8c
franswah <jfrancis2795@gmail.com>
2017-05-16 22:48:25 -0700

    lab 4 done

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Controller2D))]
+public class PlayerManager : MonoBehaviour {
+
+	public float jumpHeight = 4;
+	public float timeToJumpApex = .4f;
+	public float accelerationTimeAirborne = .2f;
+	public float accelerationTimeGrounded = .1f;
+ 	public float moveSpeed = 6;
+
+	float gravity;
+	float jumpVelocity;
+	Vector3 velocity;
+	float velocityXSmoothing;
+	bool facingRight = true;
+
+	Controller2D controller;
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<Controller2D> ();
+		anim = GetComponent<Animator> ();
+
+		gravity = -(2 * jumpHeight) / Mathf.Pow (timeToJumpApex, 2);
+		jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(controller.collisions.above || controller.collisions.below) {
+			velocity.y = 0;
+		}
+
+		Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+
+		if (Input.GetKeyDown(KeyCode.Space) && (controller.collisions.below)) {
+			velocity.y = jumpVelocity;
+		}
+
+		float targetVelocityX = input.x * moveSpeed;
+		velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityX, ref velocityXSmoothing, 
+			(controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
+		velocity.y += gravity * Time.deltaTime;
+		controller.Move (velocity * Time.deltaTime);
+
+		if (input.x > 0 && !facingRight) {
+			Flip ();
+		}
+		else if (input.x < 0 && facingRight) {
+			Flip ();
+		}
+
+		anim.SetFloat ("Speed", Mathf.Abs(input.x));
+	}
+
+	void Flip() {
+		facingRight = !facingRight;
+
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
